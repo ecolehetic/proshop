5b8bd446bd45a2e0bfadaeaaa8ca9872d17b49b8
Valentin Commenge <txreplay@gmail.com>
2016-06-28 13:39:12 +0200

    Generate Budget Admin

---
+++ src/AppBundle/Admin/BudgetAdmin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class BudgetAdmin extends AbstractAdmin
+{
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('value', null, ['label' => 'Valeur'])
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('value', null, ['label' => 'Valeur'])
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id', null, ['label' => 'ID'])
+            ->add('value', null, ['label' => 'Valeur'])
+            ->add('createdAt', null, ['label' => 'Créé le'])
+            ->add('updatedAt', null, ['label' => 'Modifié le'])
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id', null, ['label' => 'ID'])
+            ->add('value', null, ['label' => 'Valeur'])
+        ;
+    }
+}
