bc42847cc8b25426def86c607f6ebc17adff1cec
Valentin Commenge <txreplay@gmail.com>
2016-06-29 11:21:01 +0200

    Mailer config

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -13,6 +13,23 @@ class FrontController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $name = 'Valentin';
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Hello Email')
+            ->setFrom('proshop.tech.mail@gmail.com')
+            ->setTo('manugorre@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    'emails/registration.html.twig',
+                    array('name' => $name)
+                ),
+                'text/html'
+            )
+        ;
+
+        $this->get('mailer')->send($message);
+
         return $this->render('front/index.html.twig', []);
     }
 }
